fdc801faa558c7818dd22ec31458305d0d6c1037
flowcore-aidev <w63towing@gmail.com>
2025-07-26 12:18:26 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://deployclientfinal.onrender.com"
+  credentials: true
+}));
 app.use(express.json());
 
 app.get('/health', (req, res) => {
